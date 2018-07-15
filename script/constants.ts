2760622b67d4ffad26456ecf40204cdd561a1620
HOWARD LARRISON <development@daybreak.coffee>
2018-07-15 20:33:24 +0700

    Update constants.ts

--- script/constants.ts
+++ script/constants.ts
@@ -1,6 +1,6 @@
 import path = require('path');
 
-// Paths.
+// Paths
 export const ROOT_PATH = path.join(__dirname, '..');
 export const SRC_PATH = path.join(ROOT_PATH, 'src');
 export const LOGS_PATH = path.join(ROOT_PATH, 'logs');
